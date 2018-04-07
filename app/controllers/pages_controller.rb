d4474e34014c9bbf40e778dfaaa23a33ed88cc96
LilliamA <lilliam.arce31@gmail.com>
2018-04-06 19:16:19 -0600

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def regulation
+    end
+        
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  PagesController.action_methods.each do |action|
+    get "/#{action}", to: "pages##{action}", as: "#{action}_page"
+  end
+
+end
